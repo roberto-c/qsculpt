18b42b8be85fd1cf55996ce28127d85ecc1ccf3f
roberto.cabral <roberto.cabral@c691f87f-ed2b-0410-a6f8-99e647927f8a>
2008-02-27 03:30:27 +0000

    Use VBOs in the smooth renderer.

--- qsculpt/engine/include/SmoothRenderer.h
+++ qsculpt/engine/include/SmoothRenderer.h
@@ -2,6 +2,7 @@
 #define SMOOTHRENDERER_H_
 
 #include "IRenderer.h"
+#include "BufferObject.h"
 
 class SmoothRenderer : public IRenderer
 {
@@ -10,6 +11,22 @@ public:
 	virtual ~SmoothRenderer();
 
 	virtual void renderObject(const IObject3D* mesh);
+	
+private:
+	/**
+	 * Draw the mesh using OpenGL VBOs.
+	 * The VBOs are re-build when the mesh has been changed since the last draw.
+	 */
+	void renderVbo(const IObject3D* mesh);
+	
+	/**
+	 * Draw the mesh using the glBeing()/glEnd() and friends functions.
+	 * This method is a fallback method if the  VBOs are not supported.
+	 */
+	void renderImmediate(const IObject3D* mesh);
+	
+	VertexBuffer m_vertexBuffer;
+	VertexBuffer m_normalBuffer;
 };
 
 #endif /*SMOOTHRENDERER_H_*/
--- qsculpt/engine/src/SmoothRenderer.cpp
+++ qsculpt/engine/src/SmoothRenderer.cpp
@@ -24,31 +24,39 @@
 
 SmoothRenderer::SmoothRenderer()
 {
-	//qDebug() << "SmoothRenderer constructor";
+	qDebug() << "SmoothRenderer constructor";
 }
 
 SmoothRenderer::~SmoothRenderer()
 {
-	//qDebug() << "SmoothRenderer destructor";
+	qDebug() << "SmoothRenderer destructor";
+	if (m_vertexBuffer.getBufferID() != 0)
+		m_vertexBuffer.destroy();
+	if (m_normalBuffer.getBufferID() != 0)
+		m_normalBuffer.destroy();
 }
 
 void SmoothRenderer::renderObject(const IObject3D* mesh)
 {
-    //TODO: Bound checking missing
+	renderVbo(mesh);
+}
+
+void SmoothRenderer::renderImmediate(const IObject3D* mesh)
+{
 	const FaceContainer& faceList = mesh->getFaceList();
     int size = faceList.size();
-
+	
     if (mesh->isSelected())
     	glColor3d(0.0, 1.0, 0.0);
     else
     	glColor3d(0.8, 0.8, 0.8);
-
+	
     for ( int i = 0; i < size; i++)
     {
-//        if (faceList[i].isMarked)
-//            glColor3f(1.0, 0.0, 0.0);
-//        else
-//            glColor3f(0.9, 0.9, 0.9);
+		//        if (faceList[i].isMarked)
+		//            glColor3f(1.0, 0.0, 0.0);
+		//        else
+		//            glColor3f(0.9, 0.9, 0.9);
         glBegin(GL_POLYGON);
         const Face& f = faceList[i];
         for (int j = 0; j < f.point.size(); ++j)
@@ -58,4 +66,102 @@ void SmoothRenderer::renderObject(const IObject3D* mesh)
         }
         glEnd();
     }
+}
+
+void SmoothRenderer::renderVbo(const IObject3D* mesh)
+{
+	//qDebug() << "Render as selected = " << mesh->getShowBoundingBox();
+	if (mesh == NULL)
+		return;
+	
+	bool buildVBO = false;
+	
+	if (m_vertexBuffer.getBufferID() == 0)
+	{
+		m_vertexBuffer.create();
+		buildVBO = true;
+	}
+	if (m_normalBuffer.getBufferID() == 0)
+	{
+		m_normalBuffer.create();
+		buildVBO = true;
+	}
+	
+	// Set the depth function to the correct value
+    glDepthFunc(GL_LESS);
+	
+  	// Store the transformation matrix
+  	glPushMatrix();
+  	float x = 0.0f, y = 0.0f, z = 0.0f;
+  	mesh->getPosition(&x, &y, &z);
+   	glTranslatef(x, y, z);
+	
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glEnableClientState(GL_NORMAL_ARRAY);
+	
+	IObject3D* obj = const_cast<IObject3D*>(mesh);
+	int numFaces = obj->getFaceList().size();
+	int numFloats = numFaces*4*3;	// num floats = Faces * Num of point by face
+	//				* Num elements of point
+	
+	if ((obj->hasChanged() || buildVBO ) && numFloats > 0)
+	{
+		qDebug() << "SmoothRenderer::renderObject Start time:" << QDateTime::currentDateTime();
+		qDebug() << "Num Vertices=" << obj->getPointList().size();
+		qDebug() << "Num Normals=" << obj->getNormalList().size();
+		GLfloat* vtxData = new GLfloat[numFloats];
+		GLfloat* nmlData = new GLfloat[numFloats];
+		
+		int vertexIndex;
+		for (int i = 0; i < numFaces; ++i)
+		{
+			for (int j = 0; j<4; ++j)
+			{
+				vertexIndex = obj->getFaceList().at(i).point[j];
+				vtxData[(i*12) + (j*3)] = obj->getPointList().at(vertexIndex).getX();
+				vtxData[(i*12) + (j*3) + 1] = obj->getPointList().at(vertexIndex).getY();
+				vtxData[(i*12) + (j*3) + 2] = obj->getPointList().at(vertexIndex).getZ();
+				
+				nmlData[(i*12) + (j*3)] = obj->getNormalList().at(vertexIndex).getX();
+				nmlData[(i*12) + (j*3) + 1] = obj->getNormalList().at(vertexIndex).getY();
+				nmlData[(i*12) + (j*3) + 2] = obj->getNormalList().at(vertexIndex).getZ();
+			}
+		}
+		
+		m_vertexBuffer.setBufferData((GLvoid*)vtxData, numFloats*sizeof(GL_FLOAT));
+		m_normalBuffer.setBufferData((GLvoid*)nmlData, numFloats*sizeof(GL_FLOAT));
+		
+		delete [] vtxData;
+		delete [] nmlData;
+		qDebug() << "SmoothRenderer::renderObject End time:" << QDateTime::currentDateTime();
+		
+		obj->setChanged(false);
+	}
+	
+	glBindBuffer(GL_ARRAY_BUFFER, m_vertexBuffer.getBufferID());
+	glVertexPointer(3, GL_FLOAT, 0, NULL);
+	glBindBuffer(GL_ARRAY_BUFFER, m_normalBuffer.getBufferID());
+	glNormalPointer(GL_FLOAT, 0, NULL);
+	
+	QColor color;
+	color = Qt::white; //mesh->getPointList().at(f.normal[j]).color;
+	if (mesh->isSelected())
+	{
+		glColor3d(color.redF(), color.greenF() + 0.3, color.blueF());
+	}
+	else
+	{
+		glColor3d(color.redF(), color.greenF(), color.blueF());
+	}
+	
+	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, g_ibo.getBufferID());
+	glDrawArrays(GL_QUADS, 0, obj->getFaceList().size()*4);
+	//glDrawElements(GL_QUADS, obj->getFaceList().size()*4, GL_UNSIGNED_INT, NULL);
+	
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	glDisableClientState(GL_VERTEX_ARRAY);
+	glDisableClientState(GL_NORMAL_ARRAY);
+	
+	glPopMatrix();
 }
