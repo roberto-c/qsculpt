5ab60fd522b82b748ea03f3921fb8b19dbd6f280
Roberto C <roberto.cabral@gmail.com>
2015-11-28 23:20:28 -0800

    Do not show OpenCL 2 deprecation warning. We are OCL 1.2

--- plastilinacore/include/PlastilinaCore/Stable.h
+++ plastilinacore/include/PlastilinaCore/Stable.h
@@ -9,6 +9,8 @@
 #define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
 
 // TODO: reference additional headers your program requires here
+// We are not ready to use OpenCL 2.0. So use deprecated features
+#define CL_USE_DEPRECATED_OPENCL_2_0_APIS
 
 #if defined __cplusplus
 
