2927fc874320fd38293e48ff4a4eb148aa53d302
Roberto C <roberto.cabral@gmail.com>
2017-01-02 18:47:54 -0800

    CMake build system fixes / improvements

--- cmake/Modules/FindSDL2Image.cmake
+++ cmake/Modules/FindSDL2Image.cmake
@@ -1,10 +1,23 @@
 FIND_PACKAGE(PackageHandleStandardArgs)
 
+if(CMAKE_SIZEOF_VOID_P EQUAL 8)
+  set(VC_LIB_PATH_SUFFIX lib/x64)
+else()
+  set(VC_LIB_PATH_SUFFIX lib/x86)
+endif()
+
 if (APPLE)
-	find_library(SDL2IMAGE_LIBRARIES SDL2_image DOC "SDL2 library framework for MacOS X")
+	find_library(SDL2IMAGE_LIBRARIES 
+    NAMES SDL2_image
+    DOC "SDL2 library framework for MacOS X"
+    )
 	find_path(SDL2IMAGE_INCLUDE_DIRS SDL2_image/SDL_image.h DOC "Include directory for SDL2_image under MacOS X")
 else (APPLE)
-	find_library(SDL2IMAGE_LIBRARIES SDL2_image DOC "SDL2_image library")
+	find_library(SDL2IMAGE_LIBRARIES 
+    NAMES SDL2_image 
+    DOC "SDL2_image library"
+    PATH_SUFFIXES lib ${VC_LIB_PATH_SUFFIX}
+    )
 	find_path(SDL2IMAGE_INCLUDE_DIRS SDL2/SDL_image.h DOC "Include directory for SDL2_image")
 endif (APPLE)
 
