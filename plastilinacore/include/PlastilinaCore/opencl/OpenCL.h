99469ff90a48af9d0094500e74c4b019a5342a03
Roberto C <roberto.cabral@gmail.com>
2017-04-23 19:56:41 -0700

    Target OpenCL 1.2. as some 2.0 functions seems to be failing.

--- plastilinacore/include/PlastilinaCore/opencl/OpenCL.h
+++ plastilinacore/include/PlastilinaCore/opencl/OpenCL.h
@@ -19,7 +19,7 @@
  ***************************************************************************/
 #pragma once
 
-#define CL_HPP_TARGET_OPENCL_VERSION 200
+#define CL_HPP_TARGET_OPENCL_VERSION 120
 #define CL_HPP_MINIMUM_OPENCL_VERSION 120
 #define CL_HPP_ENABLE_EXCEPTIONS
 
